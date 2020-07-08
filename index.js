9c48358c2dee203d93a43d5c54128bd80f5a3d42
Fábio Senra Corrêa <fabiosenracorrea@hotmail.com>
2020-07-08 20:45:36 -0300

    js no longer removes instruction on hover

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 function tool_tip() {
     let tool_tip = document.getElementsByClassName("tool-tip")[0];
-    let instructions_text = document.getElementsByClassName("click-instruction")[0]
     tool_tip.classList.toggle("hide")
-    instructions_text.classList.toggle("hide")
 }
 
 let tool_tip_img = document.getElementsByClassName("tool-tip-img")[0]
