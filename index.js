792b16d17307b78b4f7af8075df12da5f8daacc8
Fábio Senra Corrêa <fabiosenracorrea@hotmail.com>
2020-07-15 13:23:16 -0300

    checking tool_tip on mobile fix

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 function tool_tip() {
     let tool_tip = document.getElementsByClassName("tool-tip")[0];
-    tool_tip.classList.toggle("hide")
+    tool_tip.classList.toggle("hide");
+}
+
+function tool_tip_out() {
+    let tool_tip = document.getElementsByClassName("tool-tip")[0];
+    if (tool_tip.classList.contains('hide')) {
+      return;
+  } else {
+    tool_tip.classList.toggle("hide");
+  }
 }
 
 let tool_tip_img = document.getElementsByClassName("tool-tip-img")[0]
 tool_tip_img.setAttribute("onmouseover", "tool_tip()")
-tool_tip_img.setAttribute("onmouseout", "tool_tip()")
-tool_tip_img.setAttribute("onclick", "tool_tip()")
+tool_tip_img.setAttribute("onmouseout", "tool_tip_out()")
+
